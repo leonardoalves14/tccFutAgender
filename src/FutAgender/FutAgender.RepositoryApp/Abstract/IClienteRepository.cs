5bf54631bfcf6a0e57a2a2b86a4b2ea61b18e2d7
viniciusavansini <vi.avn@hotmail.com>
2018-06-19 19:45:37 -0300

    new proj

---
+++ src/FutAgender/FutAgender.RepositoryApp/Abstract/IClienteRepository.cs
@@ -0,0 +1,11 @@
+﻿using FutAgender.RepositoryApp.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FutAgender.RepositoryApp.Abstract
+{
+    public interface IClienteRepository
+    {
+        Task<List<Cliente>> GetClientes();
+    }
+}
---
+++ src/FutAgender/FutAgender.RepositoryApp/Concrete/ClienteRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using FutAgender.RepositoryApp.Abstract;
+using FutAgender.RepositoryApp.Entities;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace FutAgender.RepositoryApp.Concrete
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        public async Task<List<Cliente>> GetClientes()
+        {
+            var clientes = new List<Cliente>();
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                await conn.OpenAsync();
+
+                clientes.AddRange(await conn.QueryAsync<Cliente>("spsCliente"));
+            }
+
+            return clientes;
+        }
+    }
+}
