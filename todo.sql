62e6e9ffc7719635192502465a17fbb8dcbe1f82
viniciusavansini <vi.avn@hotmail.com>
2018-06-30 17:02:25 -0300

    todo atualizado

--- todo.sql
+++ todo.sql
@@ -1,5 +1,7 @@
 -- Procedures feitas:
--- Cliente     -> CRUD/E
--- Endereco    -> E
--- Cargo       -> CRUD/E
--- Funcionario -> CR
+-- Cliente         -> CRUD/E
+-- Endereco        -> E
+-- Cargo           -> CRUD/E
+-- Funcionario     -> CRUD/E
+-- Estabelecimento -> CRUD/E
+-- Usuario         -> CRUD
