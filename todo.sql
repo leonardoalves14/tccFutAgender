60cf92e3ed4bc73b74c19041e61003c393cdc078
Vinícius de Souza Avansini <vi.avn@hotmail.com>
2018-07-11 16:24:32 -0300

    criptografia de senha

--- todo.sql
+++ todo.sql
@@ -8,3 +8,26 @@
 -- Horário         -> CRUD
 -- Agendamento     -> CRD
 
+/// <summary>
+        /// Metodo responsavel pela criptografia SHA1
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private string SHA1Crypto(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            var hashedData = sha.ComputeHash(Encoding.Unicode.GetBytes(value));
+            var sha1text = string.Empty;
+
+            foreach (var b in hashedData)
+            {
+                sha1text += string.Format("{0,2:X2}", b);
+            }
+
+            return sha1text;
+        }
